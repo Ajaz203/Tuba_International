c679ea72aa5db8ed490fee2b499343ced0ffbaed
ajaz203 <ajazahmad203@gmail.com>
2024-12-10 03:43:15 +0530

    updates

--- src/app/components/visa/instant-visa/instant-visa.component.ts
+++ src/app/components/visa/instant-visa/instant-visa.component.ts
@@ -1,18 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { PagesService } from '../../../shared/services/pages.service';
 import { LayoutComponent } from '../../../shared/components/ui/layout/layout.component';
 import { FooterComponent } from '../../../shared/components/footer/footer.component';
 import { BreadcrumbsComponent } from '../../../shared/components/comman/breadcrumbs/breadcrumbs.component';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { TouristcardComponent } from '../touristcard/touristcard.component';
 
 @Component({
   selector: 'app-instant-visa',
   standalone: true,
   providers: [PagesService],
   templateUrl: './instant-visa.component.html',
- styleUrl:'./instant-visa.component.scss',
-  imports: [LayoutComponent, FooterComponent, BreadcrumbsComponent, HeaderComponent]
+  styleUrls: ['./instant-visa.component.scss',], 
+  imports: [LayoutComponent, FooterComponent, BreadcrumbsComponent, HeaderComponent, TouristcardComponent] 
 })
 export class InstantVisaComponent {
-
+  @Input() image!: string; 
+  @Input() location!: string;
+  @Input() date!: string;
+  @Input() time!: string
+  cards = [
+    {
+      image: '/assets/imges2/japan.jpg',
+      location: 'Iceland',
+      date: '24 Dec',
+      time: '10:25 PM',
+    },
+    {
+      image: '/assets/imges2/japan.jpg',
+      location: 'China',
+      date: '19 Dec',
+      time: '10:25 PM',
+    },
+    {
+      image: '/assets/imges2/japan.jpg',
+      location: 'United Arab Emirates',
+      date: '14 Dec',
+      time: '10:25 PM',
+    },
+    {
+      image: '/assets/imges2/japan.jpg',
+      location: 'Czech Republic',
+      date: '28 Dec',
+      time: '10:25 PM',
+    },
+    // Add more cards here
+  ];
 }
