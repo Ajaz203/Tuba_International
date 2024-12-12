89fcfae28c2e61f9f3d219aa694b26d615684403
ajaz203 <ajazahmad203@gmail.com>
2024-12-13 01:35:43 +0530

    updates

--- src/app/components/visa/apply-visa/apply-visa.component.ts
+++ src/app/components/visa/apply-visa/apply-visa.component.ts
@@ -3,15 +3,17 @@ import { HeaderComponent } from '../../../shared/components/header/header.compon
 import { FooterComponent } from '../../../shared/components/footer/footer.component';
 import { LayoutComponent } from '../../../shared/components/ui/layout/layout.component';
 import { PagesService } from '../../../shared/services/pages.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-apply-visa',
   standalone: true,
   providers: [PagesService],
   templateUrl: './apply-visa.component.html',
   styleUrl: './apply-visa.component.scss',
-  imports:[HeaderComponent,FooterComponent,LayoutComponent]
+  imports:[HeaderComponent,FooterComponent,LayoutComponent,ReactiveFormsModule,CommonModule]
 })
 export class ApplyVisaComponent {
-
-
+ 
 }
---
+++ src/app/components/visa/visa.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  // ...
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+    // ... other imports
+  ],
+  // ...
+})
+export class VisaModule { }
